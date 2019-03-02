b943587bd82e4a9e6e2d162b9abfd3919e2f3c82
Zhihao Wang <zhihaow@amazon.com>
2019-03-02 17:02:27 +0000

    foo

--- computer_science/survey/stream_by_products/Flink/Lambda.scala
+++ computer_science/survey/stream_by_products/Flink/Lambda.scala
@@ -9,17 +9,25 @@ senv.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
 
 // Due to scala Class.getSimpleName bug https://github.com/scala/bug/issues/2034 , don't use case class.
 // Dummy Ascending Timestamps and Watermark for simplicity.
-val absoluteStream = senv.fromElements(("jim", 12, Timestamp.valueOf("2019-02-20 00:00:00")), ("Amy", 10, Timestamp.valueOf("2019-02-28 00:00:00")), ("jim", 20, Timestamp.valueOf("2019-03-02 00:00:00"))).assignAscendingTimestamps(_._3.getTime())
-val deltaStream = senv.fromElements(("jim", 1, Timestamp.valueOf("2019-02-20 00:00:59")), ("Amy", 3, Timestamp.valueOf("2019-02-28 12:00:00")), ("jim", 8, Timestamp.valueOf("2019-03-02 00:23:00"))).assignAscendingTimestamps(_._3.getTime())
+val absoluteStream = senv.fromElements(
+    ("jim", 12, Timestamp.valueOf("2019-02-20 00:00:00")),
+    ("Amy", 10, Timestamp.valueOf("2019-02-28 00:00:00")),
+    ("jim", 20, Timestamp.valueOf("2019-03-02 00:00:00"))
+).assignAscendingTimestamps(_._3.getTime())
+
+val deltaStream = senv.fromElements(
+    ("jim", 1, Timestamp.valueOf("2019-02-20 00:00:59")),
+    ("Amy", 3, Timestamp.valueOf("2019-02-28 12:00:00")),
+    ("jim", 8, Timestamp.valueOf("2019-03-02 00:23:00"))
+).assignAscendingTimestamps(_._3.getTime())
+
 val absoluteTable = absoluteStream.toTable(stenv, 'merchant, 'skus, 'snapshot_date.rowtime)
 val deltaTable = deltaStream.toTable(stenv, 'merchant, 'skus, 'snapshot_date.rowtime)
-val latestAbsolute = absoluteTable.createTemporalTableFunction('snapshot_date, 'merchant) 
+val latestAbsolute = absoluteTable.createTemporalTableFunction('snapshot_date, 'merchant)
 
 stenv.registerFunction("LatestAbsolute", latestAbsolute)
 stenv.registerTable("Absolute", absoluteTable)
 stenv.registerTable("Delta", deltaTable)
-
-
 
 // Use Dynamic Table and Time-windowed Join to implement Lambda Architechture on Flink
 stenv.sqlQuery("select a.merchant, a.skus, a.snapshot_date, a.skus + sum(d.skus) as real_time_skus from Absolute a, Delta d where a.merchant = d.merchant and d.snapshot_date > a.snapshot_date group by a.merchant, a.skus, a.snapshot_date").toRetractStream[Row].print()
