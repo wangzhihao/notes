de951ec6bd3dcf1165be30b71ab43c915d984567
Zhihao Wang <zhihaow@amazon.com>
2019-03-28 07:07:10 +0000

    foo

---
+++ computer_science/survey/stream_by_products/Flink/examples/FollowingCumulativeSum.scala
@@ -0,0 +1,48 @@
+/**
+ * We need to setup the socket first
+ * Shell> nc -l 9234 
+ *
+ * This code run with flink scala shell in local mode as follows:
+ * Shell> ./build-target/bin/start-scala-shell.sh local
+ *
+ * Sample Input
+ * Shell> nc -l 9234
+ * 1
+ * 2
+ * 4
+ * 8
+ *
+ * Sample Output
+ * (true,1,1)
+ * (true,2,3)
+ * (true,4,7)
+ * (true,8,15)
+ *
+ */
+
+val numbers = senv.socketTextStream("localhost", 9234, '\n')
+val numbersTable = numbers.toTable(stenv, 'number, 'snapshot_date.proctime)
+stenv.registerTable("Numbers", numbersTable)
+
+val sumTable = stenv.sqlQuery("select sum(cast(number as int)) as total from Numbers n")
+stenv.registerTable("SumNumbers", sumTable)
+
+val precedingCumulativeSumTable = stenv.sqlQuery("""
+     | select 
+     |   cast(number as int) as number,
+     |   sum(cast(number as int)) over (order by snapshot_date range unbounded preceding) as cumulative_numbers
+     | from Numbers n
+     """.stripMargin)
+stenv.registerTable("PrecedingNumbers", precedingCumulativeSumTable)
+
+val followingCumulativeSumTable = stenv.sqlQuery("""
+     | select 
+     |   total - cumulative_numbers as following_cumulative_numbers
+     | from SumNumbers s, PrecedingNumbers p 
+     | where s.snapshot_date = p.snapshot_date
+     """.stripMargin)
+
+followingCumulativeSumTable.toRetractStream[Row].print()
+
+
+senv.execute("My streaming program")
