1716451fd0045a6199f3548d2b9f2576cf3388cc
Zhihao Wang <zhihaow@amazon.com>
2019-03-31 14:26:11 +0000

    foo

--- computer_science/survey/stream_by_products/Flink/examples/InventoryAge_Join.scala
+++ computer_science/survey/stream_by_products/Flink/examples/InventoryAge_Join.scala
@@ -7,27 +7,37 @@
  * Shell> ./build-target/bin/start-scala-shell.sh local
  */
 
-// 1,1,X00,100,2019-03-28 00:00:00
+// 1,1,X00,100,2019-03-28
 val inventory = senv.socketTextStream("localhost", 9234, '\n').map(x => {
     val split = x.split(",")
-    val format = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    val format = new java.text.SimpleDateFormat("yyyy-MM-dd")
     val eventTime = new java.sql.Timestamp(format.parse(split(4)).getTime())
     (split(0).toInt, split(1).toInt, split(2), split(3).toInt, eventTime)
 })
 val inventoryTable = inventory.toTable(stenv, 'merchant_id, 'marketplace_id, 'fnsku, 'quantity, 'event_time, 'proc_time.proctime)
 stenv.registerTable("Inventory", inventoryTable)
 
-// 1,1,X00,50,2018-03-26 00:00:00
-// 1,1,X00,25,2018-03-25 00:00:00
-// 1,1,X00,100,2018-03-24 00:00:00
+// 1,1,X00,50,2018-03-26
+// 1,1,X00,25,2018-03-25
+// 1,1,X00,100,2018-03-24
 val event = senv.socketTextStream("localhost", 9235, '\n').map(x => {
     val split = x.split(",")
-    val format = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    val format = new java.text.SimpleDateFormat("yyyy-MM-dd")
     val eventTime = new java.sql.Timestamp(format.parse(split(4)).getTime())
     (split(0).toInt, split(1).toInt, split(2), split(3).toInt, eventTime)
 })
-val eventTable = event.toTable(stenv, 'merchant_id, 'marketplace_id, 'fnsku, 'quantity, 'event_time, 'proc_time.proctime)
+val rawEventTable = event.toTable(stenv, 'merchant_id, 'marketplace_id, 'fnsku, 'quantity, 'event_time, 'proc_time.proctime)
+stenv.registerTable("RawEvent", rawEventTable)
+
+val eventTable = stenv.sqlQuery("""
+   | select
+   |   merchant_id, marketplace_id, fnsku, event_time, sum(quantity) as quantity
+   | from RawEvent
+   | group by merchant_id, marketplace_id, fnsku, event_time
+    """.stripMargin)
+
 stenv.registerTable("Event", eventTable)
+
 
 // Event table should be maintained small to avoid a huge state.
 // The cumulative sum range [a.event_time, now], inclusive.
@@ -36,7 +46,7 @@ val eventCumulativeSumTable = stenv.sqlQuery("""
     |   a.merchant_id, a.marketplace_id, a.fnsku, a.event_time, a.quantity,
     |   sum(b.quantity) as cumulative_quantity
     | from Event a
-    | left join Event b
+    | join Event b
     | on
     | a.merchant_id = b.merchant_id
     | and a.marketplace_id = b.marketplace_id
@@ -53,8 +63,8 @@ val inventoryAgeTable = stenv.sqlQuery("""
      | select
      |   I.merchant_id, I.marketplace_id, I.fnsku,
      |   case when E.cumulative_quantity > I.quantity then
-     |       case when I.quantity > E.cumulative_quantity - E.quantity then 
-     |         I.quantity - E.cumulative_quantity + E.quantity 
+     |       case when I.quantity > E.cumulative_quantity - E.quantity then
+     |         I.quantity - E.cumulative_quantity + E.quantity
      |       else 0 end
      |   else
      |       E.quantity
