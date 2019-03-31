b426b4ae92cab85f663c48c092cd951af223899f
Zhihao Wang <zhihaow@amazon.com>
2019-03-31 11:22:56 +0000

    foo

--- computer_science/survey/stream_by_products/Flink/examples/InventoryAge.scala
+++ computer_science/survey/stream_by_products/Flink/examples/InventoryAge.scala
@@ -10,62 +10,67 @@
  * two streams: a sum stream and a preceding unbounded window stream. Substract them gives the answer.
  */
 
-val inventory = senv.socketTextStream("localhost", 9234, '\n')
-val inventoryTable = numbers.toTable(stenv, 'merchant_id, 'marketplace_id, 'fnsku, 'total_quantity, 'event_time.proctime)
+val inventory = senv.socketTextStream("localhost", 9234, '\n').map(x => {
+    val split = x.split(",")
+    (split(0).toInt, split(1).toInt, split(2), split(3).toDouble, split(4))
+})
+val inventoryTable = inventory.toTable(stenv, 'merchant_id, 'marketplace_id, 'fnsku, 'quantity, 'event_time, 'proc_time.proctime)
 stenv.registerTable("Inventory", inventoryTable)
 
-val event = senv.socketTextStream("localhost", 9235, '\n')
-val eventTable = numbers.toTable(stenv, 'merchant_id, 'marketplace_id, 'fnsku, 'quantity, 'event_type, 'event_time.proctime)
+// 1,1,X00,53.2,2018-01-01
+val event = senv.socketTextStream("localhost", 9235, '\n').map(x => {
+    val split = x.split(",")
+    (split(0).toInt, split(1).toInt, split(2), split(3).toDouble, split(4))
+})
+val eventTable = event.toTable(stenv, 'merchant_id, 'marketplace_id, 'fnsku, 'quantity, 'event_time, 'proc_time.proctime)
 stenv.registerTable("Event", eventTable)
 
-val inflowSumTable = stenv.sqlQuery("""
+val eventSumTable = stenv.sqlQuery("""
       | select
       | merchant_id, marketplace_id, fnsku,
-      | sum(cast(quantity as int)) as event_total_quantity
+      | sum(quantity) as event_total_quantity
       | from Event
-      | where event_type = 'inflow'
+      | group by merchant_id, marketplace_id, fnsku
       """)
-stenv.registerTable("InflowSum", inflowSumTable)
+stenv.registerTable("EventSum", eventSumTable)
 
-val inflowFollowingCumulativeSumTable = stenv.sqlQuery("""
+
+val eventPrecedingCumulativeSumTable = stenv.sqlQuery("""
      | select
-     |   merchant_id, marketplace_id, fnsku,
-     |   sum(cast(quantity as int)) over (order by snapshot_date range unbounded preceding) as cumulative_quantity,
+     |   merchant_id, marketplace_id, fnsku, quantity,
+     |   sum(cast(quantity as int)) over (order by proc_time range unbounded preceding) as cumulative_quantity,
      |   event_time
      | from Event
-     | where event_type = 'inflow'
      """.stripMargin)
-stenv.registerTable("InflowFollowingSum", inflowFollowingCumulativeSumTable)
+stenv.registerTable("EventPrecedingCumulativeSum", eventPrecedingCumulativeSumTable)
 
-val inflowFollowingCumulativeSumTable = stenv.sqlQuery("""
+val eventFollowingCumulativeSumTable = stenv.sqlQuery("""
      | select
-     |   p.merchant_id, p.marketplace_id, p.fnsku,
+     |   p.merchant_id, p.marketplace_id, p.fnsku, p.quantity,
      |   s.event_total_quantity - p.cumulative_quantity as cumulative_quantity,
      |   p.event_time
-     | from InflowSum s, InflowFollowingSum p
+     | from EventSum s, EventPrecedingCumulativeSum p
      | where s.merchant_id = p.merchant_id
      | and s.marketplace_id = p.marketplace_id
      | and s.fnsku = p.fnsku
      """.stripMargin)
 
-stenv.registerTable("InflowFollowingSum", inflowFollowingCumulativeSumTable)
+stenv.registerTable("EventFollowingCumulativeSum", eventFollowingCumulativeSumTable)
 
 val inventoryAgeTable = stenv.sqlQuery("""
      | select
      |   I.merchant_id, I.marketplace_id, I.fnsku,
-     |   case when E.quantity > I.total_quantity - E.cumulative_quantity then
-     |       case when I.total_quantity - E.cumulative_quantity > 0 then I.total_quantity - E.cumulative_quantity else 0 end
+     |   case when E.quantity > I.quantity - E.cumulative_quantity then
+     |       case when I.quantity - E.cumulative_quantity > 0 then I.quantity - E.cumulative_quantity else 0 end
      |   else
      |       E.quantity
      |   end as quantity,
      |   E.event_time
-     | from InventoryAge I, InflowFollowingSum E
+     | from Inventory I, EventFollowingCumulativeSum E
      | where I.merchant_id = E.merchant_id
      | and I.marketplace_id = E.marketplace_id
      | and I.fnsku = E.fnsku
      """.stripMargin)
 
-followingCumulativeSumTable.toRetractStream[Row].print()
-
-
+inventoryAgeTable.toRetractStream[Row].print()
 senv.execute("My streaming program")
