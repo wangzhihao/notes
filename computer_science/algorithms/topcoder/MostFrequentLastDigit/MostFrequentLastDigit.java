e9f6b6e51e2637b4b4e61fc38e204a585997d6d9
Zhihao Wang <zhihaow@amazon.com>
2019-04-03 07:44:00 +0000

    foo

--- computer_science/algorithms/topcoder/MostFrequentLastDigit/MostFrequentLastDigit.java
+++ computer_science/algorithms/topcoder/MostFrequentLastDigit/MostFrequentLastDigit.java
@@ -4,17 +4,12 @@ import java.io.*;
 public class MostFrequentLastDigit {
 	public int[] generate(int n, int d) {
         int [] res = new int[n];
-        if(d == 0) {
-            for(int i = 0; i < n; i++) {
-                res[i] = i*10 + 5;
-            }
-        } else {
-            for(int i = 0; i < n/2; i++) {
-                res[i] = i * 10 + d; 
-            }
-            for(int i = n/2; i < n; i++) {
-                res[i] = i * 10; 
-            }
+        int sum = d == 9 ? d : 10 + d, left_d = sum/2, right_d = sum - left_d;
+        for(int i = 0; i < n/2; i++) {
+            res[i] = i * 10 + left_d; 
+        }
+        for(int i = n/2; i < n; i++) {
+            res[i] = i * 10 + right_d; 
         }
         return res;
     }
