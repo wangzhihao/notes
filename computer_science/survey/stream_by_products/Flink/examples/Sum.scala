01e5760346f9fbd0e0791267352dc20583e18744
Zhihao Wang <zhihaow@amazon.com>
2019-05-05 03:49:11 +0000

    foo

--- computer_science/survey/stream_by_products/Flink/examples/Sum.scala
+++ computer_science/survey/stream_by_products/Flink/examples/Sum.scala
@@ -9,6 +9,37 @@ val numbers = senv.socketTextStream("localhost", 9234, '\n')
 val numbersTable = numbers.toTable(stenv, 'number)
 stenv.registerTable("Numbers", numbersTable)
 
+/*
+scala> stenv.explain(table)
+res2: String =
+"== Abstract Syntax Tree ==
+LogicalAggregate(group=[{}], EXPR$0=[SUM($0)])
+  LogicalProject($f0=[CAST($0):INTEGER])
+    LogicalTableScan(table=[[Numbers]])
+
+== Optimized Logical Plan ==
+DataStreamGroupAggregate(select=[SUM($f0) AS EXPR$0])
+  DataStreamCalc(select=[CAST(number) AS $f0])
+    DataStreamScan(table=[[_DataStreamTable_0]])
+
+== Physical Execution Plan ==
+Stage 1 : Data Source
+        content : collect elements with CollectionInputFormat
+
+        Stage 2 : Operator
+                content : from: (number)
+                ship_strategy : REBALANCE
+
+                Stage 3 : Operator
+                        content : select: (CAST(number) AS $f0)
+                        ship_strategy : FORWARD
+
+                        Stage 5 : Operator
+                                content : select: (SUM($f0) AS EXPR$0)
+                                ship_strategy : HASH
+
+"
+ */ 
 stenv.sqlQuery("select sum(cast(number as int)) from Numbers n").toRetractStream[Row].print()
 
 senv.execute("My streaming program")
