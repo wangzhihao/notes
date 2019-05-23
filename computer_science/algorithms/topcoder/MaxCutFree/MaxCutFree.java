43a855c2d6a59ac4352d2e2ab5e39a9135e2b94e
Zhihao Wang <zhihaow@amazon.com>
2019-05-23 00:34:44 +0000

    foo

--- computer_science/algorithms/topcoder/MaxCutFree/MaxCutFree.java
+++ computer_science/algorithms/topcoder/MaxCutFree/MaxCutFree.java
@@ -182,10 +182,10 @@ public class MaxCutFree {
                     5 6
                     0 1
                     1 2
-                    2 0
-                    0 3
+                    2 3
                     3 4
-                    4 0
+                    2 0
+                    4 2
                  */
                 bfs(node);
             }
